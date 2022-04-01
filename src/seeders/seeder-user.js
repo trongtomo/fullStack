fb6735ca459ba69535d08bcfbee3b957e29b4d28
trongtomo <binguyenquoctrong@gmail.com>
2022-04-01 13:41:27 +0700

    #23

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -4,9 +4,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
+        email: "trong@gmail.com",
+        password: "123456",
         firstName: "John",
         lastName: "Doe",
-        email: "example@example.com",
+        address: "US",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
