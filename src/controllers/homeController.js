c5649d8c8ef31064f64aaee4fa23cf39b3fbf59b
trongtomo <binguyenquoctrong@gmail.com>
2022-04-05 13:45:54 +0700

    #27

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -18,13 +18,31 @@ let getCRUD = (req, res) => {
 };
 let postCRUD = async (req, res) => {
   let message = await CRUDService.createNewUser(req.body);
-  console.log(message);
   return res.send("post crud from server");
 };
 let displayGetCRUD = async (req, res) => {
   let data = await CRUDService.getAllUser({ raw: true });
   return res.render("displayUser.ejs", {
     dataTable: data,
+  });
+};
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    console.log(userData);
+    return res.render("editUser.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("Users Not Found!");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("displayUser.ejs", {
+    dataTable: allUsers,
   });
 };
 // obj= key:value
@@ -34,4 +52,6 @@ module.exports = {
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
