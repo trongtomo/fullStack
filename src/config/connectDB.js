40f12c64a32eb5bece1cc384aab2e1736da6fdf8
trongtomo <binguyenquoctrong@gmail.com>
2022-10-25 16:25:02 +0700

    update readme

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("trongfake", "root", "root", {
+const sequelize = new Sequelize("trongfake", "admin", "admin", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
